96d9157b6914967317b95c71cbe9edde7c022f31
Bogdan Bakhmat <darkout2012@gmail.com>
2023-03-21 13:58:23 +0100

    clase 109 lista

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -10,16 +10,38 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
   
   termino: string = "";
+  hayError: boolean = false;
 
   constructor(private paisService: PaisService) {
   }
 
-  buscar() {
+  // buscar() {
+  //   console.log(this.termino);
+  //   this.paisService.buscarPais(this.termino)
+  //     .subscribe((resp) => {
+  //       this.hayError = false;
+  //       console.log(resp); 
+  //   }, (err) => {
+  //     this.hayError = true;
+  //   });
+  // }
+
+
+  buscar(){
+
+    this.hayError = false;
     console.log(this.termino);
 
-    this.paisService.buscarPais(this.termino).subscribe(resp => {
-      console.log(resp);
-    })
+
+    this.paisService.buscarPais(this.termino)
+        .subscribe({
+          next: (data) => {
+            console.log(data);
+          },
+          error: (err) => {
+            this.hayError = true;
+          }
+        });
   }
 
 }
