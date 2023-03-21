a7ff68d2524ec9e659efce77e3bb90480c821e1c
Bogdan Bakhmat <darkout2012@gmail.com>
2023-03-21 20:28:11 +0100

    clase 115 lista

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -7,5 +9,30 @@ import { Component } from '@angular/core';
   ]
 })
 export class PorCapitalComponent {
+
+  termino: string = "";
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService) {
+  }
+
+  buscar(termino: string){
+
+    this.hayError = false;
+    this.termino = termino;
+
+
+    this.paisService.buscarCapital(this.termino)
+        .subscribe({
+          next: (paises) => {
+            this.paises = paises;
+          },
+          error: (err) => {
+            this.hayError = true;
+            this.paises = [];
+          }
+        });
+  }
 
 }
