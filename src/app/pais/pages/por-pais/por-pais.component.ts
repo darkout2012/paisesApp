abc97a63d1f4bd91a1032a322c33b65543ae98c8
Bogdan Bakhmat <darkout2012@gmail.com>
2023-03-21 14:16:53 +0100

    clase 110 lista

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -35,9 +35,8 @@ export class PorPaisComponent {
 
     this.paisService.buscarPais(this.termino)
         .subscribe({
-          next: (data) => {
-            console.log(data);
-            data[0].
+          next: (paises) => {
+            console.log(paises);
           },
           error: (err) => {
             this.hayError = true;
